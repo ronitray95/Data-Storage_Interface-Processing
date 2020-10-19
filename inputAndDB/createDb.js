1a5e9801f98d04bbf4fc570b28a6380e9c7015a3
Akashdeep <akashdeepsingh98@gmail.com>
2020-10-19 16:05:15 +0530

    can store bibtex in mongo

--- inputAndDB/createDb.js
+++ inputAndDB/createDb.js
@@ -12,11 +12,20 @@ client.connect(function (err) {
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
-    insertDocuments(db, function () {
-        findDocuments(db, function () {
+    //insertDocuments(db, function () {
+    //    updateDocument(db, function () {
+    //        removeDocument(db, function () {
+    //            client.close();
+    //        });
+    //    });
+    //});
+    //insertDocuments(db, function () {
+    findDocuments(db, function () {
+        removeDocument(db, function () {
             client.close();
         });
     });
+    //});
 });
 
 const insertDocuments = function (db, callback) {
@@ -39,5 +48,26 @@ const findDocuments = function (db, callback) {
         console.log("Found the following records");
         console.log(docs)
         callback(docs);
+    });
+}
+
+const updateDocument = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.updateOne({ a: 2 }
+        , { $set: { b: 1 } }, function (err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            console.log("Updated the document with the field a equal to 2");
+            callback(result);
+        });
+}
+
+const removeDocument = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.deleteMany({}, function (err, result) {
+        assert.equal(err, null);
+        //assert.equal(1, result.result.n);
+        console.log("Removed the document with the field a equal to 3");
+        callback(result);
     });
 }
--- inputAndDB/index.js
+++ inputAndDB/index.js
@@ -1,6 +1,71 @@
 const bibtexParse = require('bibtex-parse');
 const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const fs = require('fs');
+
 const bibtex = fs.readFileSync('ACMbibtexs.txt', 'utf8');
-//console.log(bibtexParse.entries(bibtex)[0]);
-var myJSON = JSON.stringify(bibtexParse.entries(bibtex));
+//var btJSON = JSON.stringify(bibtexParse.entries(bibtex));
+var bt = bibtexParse.entries(bibtex);
+console.log(typeof(bt));
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'myproject';
+const client = new MongoClient(url);
+var deletion = false;
+
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    insertDocuments(db, bt, function () {
+        findDocuments(db, function () {
+            if (deletion)
+                removeDocument(db, function () { });
+            client.close();
+        });
+    });
+});
+
+const insertDocuments = function (db, data, callback) {
+    const collection = db.collection('documents');
+    collection.insertMany(data, function (err, result) {
+        assert.equal(err, null);
+        //assert.equal(3, result.result.n);
+        //assert.equal(3, result.ops.length);
+        console.log(`Inserted ${result.result.length} documents into the collection`);
+        callback(result);
+    });
+}
+
+const findDocuments = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.find({}).toArray(function (err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs)
+        callback(docs);
+    });
+}
+
+const updateDocument = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.updateOne({ a: 2 }
+        , { $set: { b: 1 } }, function (err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            console.log("Updated the document with the field a equal to 2");
+            callback(result);
+        });
+}
+
+const removeDocument = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.deleteMany({}, function (err, result) {
+        assert.equal(err, null);
+        //assert.equal(1, result.result.n);
+        console.log("Removed the document with the field a equal to 3");
+        callback(result);
+    });
+}
