7a5695d0f53bcc0681a130816e180f0764a0bf1b
Akashdeep <akashdeepsingh98@gmail.com>
2020-10-19 13:10:03 +0530

    practicing mongo

--- inputAndDB/createDb.js
+++ inputAndDB/createDb.js
@@ -1,8 +1,43 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/playbtdb";
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'myproject';
+
+const client = new MongoClient(url);
+
+client.connect(function (err) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    insertDocuments(db, function () {
+        findDocuments(db, function () {
+            client.close();
+        });
+    });
 });
+
+const insertDocuments = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.insertMany([
+        { a: 1 }, { a: 2 }, { a: 3 }
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
+
+const findDocuments = function (db, callback) {
+    const collection = db.collection('documents');
+    collection.find({}).toArray(function (err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs)
+        callback(docs);
+    });
+}
