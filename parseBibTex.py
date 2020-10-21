e6dad7a6c6c8ad4095cd2eb4ef70a043391c591d
Akashdeep <akashdeepsingh98@gmail.com>
2020-10-21 16:39:24 +0530

    uploading file, storing data

--- parseBibTex.py
+++ parseBibTex.py
@@ -2,47 +2,45 @@
 import bibtexparser as btp
 from bibtexparser.bparser import BibTexParser as parseOpts
 from pymongo import MongoClient, errors
-from flask import request, redirect, Response, Flask,render_template
+from flask import request, redirect, Response, Flask, render_template, url_for
 app = Flask(__name__, template_folder='./')
-
-parser = parseOpts(common_strings=True)
-# dbuser=admin, pass=admin123
-client = MongoClient(
-    "mongodb+srv://admin:admin123@cluster0.ajwby.mongodb.net/testDB?retryWrites=true&w=majority")
-# parser.homogenize_fields=True
-with open("inputBibTex/SSD bibtexts ACM.txt") as bibtex_file:
-    # bib_database = btp.load(bibtex_file) #use this for well defined strings, entries like month=Jan will raise Exception
-    # put partial =false for default behaviour
-    bib_database = parser.parse_file(file=bibtex_file, partial=True)
-
-# print(bib_database.strings)
-# print(bib_database.entries)
-try:
-    client.server_info()
-    print("server_info():", "OK")
-
-except errors.ServerSelectionTimeoutError as err:
-    # catch pymongo.errors.ServerSelectionTimeoutError
-    print("pymongo ERROR:", err)
-
-db = client["testDB"]
-bibTexDB = db['bibTex']
-
-bibTexDB.insert_many(bib_database.entries)
-
-client.close()
-
-
-@app.route('/giveData', methods=['POST'])
-def giveData():
-    data = request.form['data']
-    print('The data is: '+data)
-    return render_template('index.html')
 
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def giveData():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+        parser = parseOpts(common_strings=True)
+        client = MongoClient(
+            "mongodb+srv://admin:admin123@cluster0.ajwby.mongodb.net/testDB?retryWrites=true&w=majority")
+        with open(uploaded_file.filename, encoding='utf-8') as bibtex_file:
+            bib_database = parser.parse_file(file=bibtex_file, partial=True)
+
+        try:
+            client.server_info()
+            print("server_info():", "OK")
+
+        except errors.ServerSelectionTimeoutError as err:
+            print("pymongo ERROR:", err)
+
+        db = client["testDB"]
+        bibTexDB = db['bibTex']
+
+        # gonna delete everything then insert
+        x = bibTexDB.delete_many({})
+        print(x.deleted_count, " docs deleted")
+
+        y = bibTexDB.insert_many(bib_database.entries)
+        print(len(y.inserted_ids), " docs inserted")
+
+        client.close()
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
